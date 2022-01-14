f40526b1c12318bfc7a5a1ef12012494e118ac60
Leandro <leandroquiroga9514@gmail.com>
2022-01-14 18:53:22 -0300

    List of person components

---
+++ src/components/Listado de Pacientes/components/Card.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+// import { Fragment } from 'react/cjs/react.production.min'
+
+export const Card = () => {
+    return (
+        <article className='grid grid-cols-card place-content-center gap-3'>
+            <div className='font-poppins px-5 m-2 py-10 w-full bg-white shadow-md'>
+                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
+                    <span className='normal-case'> Pedro </span>
+                </p>
+
+                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
+                    <span className='normal-case'> Pedro </span>
+                </p>
+                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
+                    <span className='normal-case'> Pedro </span>
+                </p>
+                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
+                    <span className='normal-case'> Pedro </span>
+                </p>
+                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
+                    <span className='normal-case'> Pedro </span>
+                </p>
+                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
+                    <span className='normal-case'> Pedro </span>
+                </p>
+            </div>
+            <div className='font-poppins my-2 gap-3 px-5 m-2 py-10 w-full bg-white shadow-md'>
+                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
+                    <span className='normal-case'> Pedro </span>
+                </p>
+
+                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
+                    <span className='normal-case'> Pedro </span>
+                </p>
+                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
+                    <span className='normal-case'> Pedro </span>
+                </p>
+                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
+                    <span className='normal-case'> Pedro </span>
+                </p>
+                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
+                    <span className='normal-case'> Pedro </span>
+                </p>
+                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
+                    <span className='normal-case'> Pedro </span>
+                </p>
+            </div>
+        </article>
+    )
+}

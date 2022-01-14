14dac7146b43293d450817244b0c15757270b6ba
Leandro <leandroquiroga9514@gmail.com>
2022-01-14 18:52:07 -0300

    Header Components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+
+import React, { Fragment } from 'react';
+
+export const Header = () => {
+    return (
+        <header>
+            <h1 className='font-poppins font-semibold text-3xl text-center md:w-2/3 mx-auto' >
+                Control Pacientes {''}
+                <span className='text-indigo-600 font-black'>Veterinaria</span>
+            </h1>
+        </header>
+    )
+}

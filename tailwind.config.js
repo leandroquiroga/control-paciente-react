c79436b098f1d33dd8b59736484bc11e3fe73169
Leandro <leandroquiroga9514@gmail.com>
2022-01-18 21:33:31 -0300

    Configuration tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.jsx'],
+  content: ['index.html', './src/**/*.jsx'],
   theme: {
     extend: {},
     fontFamily: {
       poppins: ['Poppins'],
     },
     gridTemplateCols: {
-      card: 'repeat(2, minmax(275px, auto))'
+      cards: 'repeat(auto-fill , minmax(350px, 1fr))'
     }
   },
   plugins: [],

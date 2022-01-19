30e9c547f67e5cfdf2b9150a822fe96113c594f4
Leandro <leandroquiroga9514@gmail.com>
2022-01-18 21:22:58 -0300

    Components Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 
-import React, { Fragment } from 'react';
+import React from 'react';
 
-export const Header = () => {
+export const Header = ({title, subTitile}) => {
     return (
-        <header>
-            <h1 className='font-poppins font-semibold text-3xl text-center md:w-2/3 mx-auto' >
-                Control Pacientes {''}
-                <span className='text-indigo-600 font-black'>Veterinaria</span>
+        <header className='p-3 font-poppins'>
+            <h1 className='font-semibold text-3xl text-center md:w-2/3 mx-auto' >
+                { title } {''}
+                <span className='text-indigo-600 font-black'>{ subTitile }</span>
             </h1>
         </header>
     )

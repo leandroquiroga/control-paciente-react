6e1351157f96f3ebd31655e90a2555652cb5c7c0
Leandro <leandroquiroga9514@gmail.com>
2022-01-18 21:27:52 -0300

    Component Card: Show information clients

--- src/components/Listado de Pacientes/components/Card.jsx
+++ src/components/Listado de Pacientes/components/Card.jsx
@@ -1,50 +1,45 @@
-import React from 'react'
-// import { Fragment } from 'react/cjs/react.production.min'
+import React from 'react';
 
-export const Card = () => {
+export const Card = ({client, setClient, deleteClient, arrClients, setArrClients}) => {
+    
+    const { mascota, propietario, email, date, sintomas, id} = client
     return (
-        <article className='grid grid-cols-card place-content-center gap-3'>
-            <div className='font-poppins px-5 m-2 py-10 w-full bg-white shadow-md'>
-                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
-                    <span className='normal-case'> Pedro </span>
-                </p>
+        <article className='flex flex-col mt-12 font-poppins py-4 px-5 mx-4 bg-white shadow-lg rounded-lg' >
+            <p className='font-bold mb-3 text-gray-700 uppercase'> Mascota: {''}
+                <span className='text-gray-600 font-semibold text-base'> {mascota} </span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'> Propietario: {''}
+                <span className='text-gray-600 font-semibold text-base'> {propietario} </span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'> Contacto: {''}
+                <span className='text-gray-600 font-semibold text-base'> {email} </span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'> Fecha de alta: {''}
+                <span className='text-gray-600 font-semibold text-base'> {date} </span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'> Sintomas: {''}
+                <span className='text-gray-600 font-semibold text-base'> {sintomas} </span>
+            </p>
 
-                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
-                    <span className='normal-case'> Pedro </span>
-                </p>
-                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
-                    <span className='normal-case'> Pedro </span>
-                </p>
-                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
-                    <span className='normal-case'> Pedro </span>
-                </p>
-                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
-                    <span className='normal-case'> Pedro </span>
-                </p>
-                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
-                    <span className='normal-case'> Pedro </span>
-                </p>
-            </div>
-            <div className='font-poppins my-2 gap-3 px-5 m-2 py-10 w-full bg-white shadow-md'>
-                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
-                    <span className='normal-case'> Pedro </span>
-                </p>
-
-                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
-                    <span className='normal-case'> Pedro </span>
-                </p>
-                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
-                    <span className='normal-case'> Pedro </span>
-                </p>
-                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
-                    <span className='normal-case'> Pedro </span>
-                </p>
-                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
-                    <span className='normal-case'> Pedro </span>
-                </p>
-                <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
-                    <span className='normal-case'> Pedro </span>
-                </p>
+            <div className='flex flex-row justify-around p-2 items-center'>
+                <button 
+                    type='button'
+                    className='w-40 p-2 border-0 outline-none cursor-pointer rounded-lg bg-indigo-600 text-white hover:bg-indigo-800'
+                    onClick={() => {
+                        deleteClient(id, arrClients, setArrClients)
+                    }}
+                >
+                    Eliminar
+                </button>
+                <button 
+                    type='button'
+                    className='w-40 p-2 border-0 outline-none cursor-pointer rounded-lg bg-indigo-600 text-white hover:bg-indigo-800'
+                    onClick={() => {
+                        setClient(client)
+                    }}
+                >
+                    Editar
+                </button>
             </div>
         </article>
     )

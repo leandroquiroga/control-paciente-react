5b877f2e7b083357201ba8a924f327bdd05c3200
Leandro <leandroquiroga9514@gmail.com>
2022-01-18 21:30:05 -0300

    Component ListClient

--- src/components/Listado de Pacientes/ListClient.jsx
+++ src/components/Listado de Pacientes/ListClient.jsx
@@ -1,17 +1,37 @@
 import React from 'react'
+import { idUnique } from '../../helpers';
 import { Title } from './../Formulario/components/Title';
 import { Card } from './components/Card';
+import { CardClean } from './components/CardClean';
 
-export const ListClient = () => {
+export const ListClient = ({ arrClients, setClient, deleteClient, setArrClients }) => {
+
     return (
-        <section className='flex flex-col justify-center items-center lg:3/5 md:w-1/2'>
+        <section className='d-flex flex-col justify-center lg:w-3/5 md:w-full '>
             <Title 
                 titleText='Listado de Pacientes'
                 subtitleText='Administra tus Pacientes'
                 spanText='Citas'
             />
-
-            <Card />
+            {
+                (arrClients.length === 0) ?
+                        (
+                            <CardClean message='No se han encontrado clientes'/>
+                        )
+                    :
+                        (
+                            arrClients.map(client => (
+                                <Card
+                                    key={idUnique()}
+                                    client={client}
+                                    setClient={setClient}
+                                    deleteClient={deleteClient}
+                                    arrClients={arrClients}
+                                    setArrClients={setArrClients}
+                                />
+                            ))
+                        )
+            }
         </section>
     )
 }
